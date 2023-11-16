82db7c8cfa41eee7f6685eac3fbcffe3f658aebb
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-11-16 08:04:32 +0700

    Render ProductList with static data

--- src/components/HomePage/ProductItem.jsx
+++ src/components/HomePage/ProductItem.jsx
@@ -4,39 +4,34 @@ import React, { Component } from 'react'
 class ProductItem extends Component {
     renderStars = (rating) => {
         const stars = []
-        for (let i = 0; i < rating; i++) {
+        const maxStars = 5
+
+        for (let i = 0; i < maxStars; i++) {
+            const starClass = i < rating ? 'text-yellow-400 text-2xl' : 'text-gray-300 text-2xl'
+
             stars.push(
-                <span key={i} className="text-yellow-500" role="img" aria-label="star">
+                <span key={i} className={starClass} role="img" aria-label={i < rating ? 'star filled' : 'star empty'}>
                     &#9733;
                 </span>
             )
         }
+
         return stars
     }
-
+    formatPrice = (price) => {
+        return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price)
+    }
     render() {
+        const { product } = this.props
+
         return (
             <div className="product-item shadow-md rounded-lg overflow-hidden text-center ">
-                {/* Ảnh sản phẩm */}
-                <img
-                    className="product-image"
-                    src={'https://product.hstatic.net/1000030244/product/so_huyet_co_587441283cea427cb4c8ca78ab79c02f_grande.jpg'}
-                    alt={'Sò điệp tươi ngon'}
-                />
-
-                {/* Tên sản phẩm */}
-                <h2 className="product-name text-lg font-semibold my-2">{'Sò điệp tươi ngon'}</h2>
-
-                {/* Đánh giá (5 ngôi sao) */}
-                <div className="product-rating text-yellow-500 mb-2">{this.renderStars(5)}</div>
-
-                {/* Giá sản phẩm/đơn vị */}
-                <p className="product-price text-gray-700">
-                    {75000}/ {'200g'}
+                <img className="product-image w-full h-70 object-cover" src={product.imageUrl} alt={product.name} />
+                <h2 className="product-name text-lg font-semibold my-2">{product.name}</h2>
+                <div className="product-rating text-yellow-500 mb-2">{this.renderStars(4)}</div>
+                <p className="product-price text-gray-700 font-medium">
+                    {this.formatPrice(product.price)}/ {product.weight}
                 </p>
-
-                {/* Nút mua hàng */}
-
                 <button className="buy-button bg-amber-400 text-white px-4 py-2 rounded-md hover:bg-yellow-300 transition duration-300 my-3">
                     Mua hàng
                 </button>
--- src/components/HomePage/ProductList.jsx
+++ src/components/HomePage/ProductList.jsx
@@ -1,63 +1,53 @@
 // ProductList.js
 import React, { Component } from 'react'
+import ReactPaginate from 'react-paginate'
+
 import ProductItem from './ProductItem'
 
 class ProductList extends Component {
-    //   state = {
-    //     products: [
-    //       // Dữ liệu sản phẩm mẫu, có thể thay đổi theo nhu cầu của bạn
-    //       // ...
-    //     ],
-    //     currentPage: 1,
-    //     productsPerPage: 12,
-    //   };
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentPage: 0,
+            productsPerPage: 12, // Số lượng sản phẩm trên mỗi trang
+        }
+    }
 
-    //   renderProducts = () => {
-    //     const { products, currentPage, productsPerPage } = this.state;
-    //     const indexOfLastProduct = currentPage * productsPerPage;
-    //     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    //     const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
-
-    //     return currentProducts.map((product) => (
-    //       <ProductItem key={product.id} {...product} />
-    //     ));
-    //   };
-
-    //   handlePageChange = (page) => {
-    //     this.setState({
-    //       currentPage: page,
-    //     });
-    //   };
-
-    //   renderPagination = () => {
-    //     const { products, currentPage, productsPerPage } = this.state;
-    //     const totalPages = Math.ceil(products.length / productsPerPage);
-
-    //     return (
-    //       <div className="flex justify-center mt-4">
-    //         {Array.from({ length: totalPages }).map((_, index) => (
-    //           <button
-    //             key={index}
-    //             className={`mx-2 p-2 rounded-full ${
-    //               currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
-    //             }`}
-    //             onClick={() => this.handlePageChange(index + 1)}
-    //           >
-    //             {index + 1}
-    //           </button>
-    //         ))}
-    //       </div>
-    //     );
-    //   };
-
+    handlePageChange = ({ selected }) => {
+        this.setState({
+            currentPage: selected,
+        })
+    }
     render() {
-        return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 item items-center mt-10 mb-10">
-                {/* {this.renderProducts()}
-        {this.renderPagination()} */}
+        const { products } = this.props
+        const { currentPage, productsPerPage } = this.state
 
-                <ProductItem />
-            </div>
+        const indexOfLastProduct = (currentPage + 1) * productsPerPage
+        const indexOfFirstProduct = indexOfLastProduct - productsPerPage
+        const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct)
+
+        return (
+            <>
+                <h3 className="my-5 text-2xl font-semibold text-gray-700 text-shadow-md">Danh sách sản phẩm</h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 item items-center mt-5 mb-10">
+                    {currentProducts.map((product) => (
+                        <ProductItem key={product.id} product={product} />
+                    ))}
+                </div>
+                <ReactPaginate
+                    pageCount={Math.ceil(products.length / productsPerPage)}
+                    pageRangeDisplayed={2}
+                    marginPagesDisplayed={1}
+                    onPageChange={this.handlePageChange}
+                    containerClassName="pagination flex justify-center"
+                    activeClassName="bg-blue-800 text-white"
+                    pageClassName="mx-2 px-3 py-2 bg-gray-200 rounded-md hover:bg-gray-300 cursor-pointer"
+                    previousLabel="Previous"
+                    nextLabel="Next"
+                    previousClassName="mx-2 px-3 py-2 bg-gray-200 rounded-md hover:bg-gray-300 cursor-pointer"
+                    nextClassName="mx-2 px-3 py-2 bg-gray-200 rounded-md hover:bg-gray-300 cursor-pointer"
+                />
+            </>
         )
     }
 }
