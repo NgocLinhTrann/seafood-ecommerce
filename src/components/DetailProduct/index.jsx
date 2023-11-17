b895fa31ec4452dba3e487f8049ca2b7fc25b42d
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-11-17 16:33:00 +0700

    Edit animation DetailProduct

--- src/components/DetailProduct/index.jsx
+++ src/components/DetailProduct/index.jsx
@@ -6,6 +6,7 @@ import ConsBages from '../HomePage/ConsBages'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import ProductList from '../HomePage/ProductList'
+import LoadingProduct from '../HomePage/LoadingProduct'
 
 export default function Index() {
     const { id } = useParams()
@@ -26,23 +27,36 @@ export default function Index() {
         try {
             const response = await fetch('https://daohaisan.azurewebsites.net/api/products')
             const data = await response.json()
-            setMoreProducts(data.data.products)
+            setMoreProducts(data.data.products.filter((item) => item.id !== id))
             console.log('haha')
             console.log(moreProducts)
         } catch (error) {
             console.error('Error fetching more products:', error)
         }
     }
+    // random array
+    const shuffleArray = (array) => {
+        const shuffled = array.slice()
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1))
+            ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+        }
+        return shuffled
+    }
 
     useEffect(() => {
         fetchData()
+
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+
+        setMoreProducts([])
+        setTimeout(() => {
+            fetchMoreProducts()
+            shuffleArray(moreProducts)
+        }, 1000)
+
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id])
-
-    useEffect(() => {
-        fetchMoreProducts()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
 
     return (
         <div className="max-w-[1200px] mx-auto">
@@ -51,11 +65,7 @@ export default function Index() {
 
             <div className="mb-10">
                 <h1 className="font-bold text-gray-700 text-2xl my-5">Các sản phẩm khác</h1>
-                {moreProducts.length === 0 ? (
-                    <div className="bg-red-100 p-4 text-gray-500 font-bold text-base">Danh mục này chưa có sản phẩm.</div>
-                ) : (
-                    <ProductList products={moreProducts} productsPerPage={4} />
-                )}
+                {moreProducts.length === 0 ? <LoadingProduct /> : <ProductList products={moreProducts} productsPerPage={4} />}
             </div>
             <ConsBages />
             <Footer />
